786250eae49bc0cad125d2c86f09dc5c7550a735
Tanner Mead <tanner_mead@comcast.net>
2025-01-29 15:17:27 -0600

    added email function after contact us submission

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
+import html
 from django.views.generic import TemplateView, FormView
 from django.urls import reverse_lazy
 
+from common.utils.email import send_email
 from .forms import ContactUsForm
 
 class HomePageView(TemplateView):
@@ -10,6 +12,24 @@ class ContactUsView(FormView):
     template_name = 'pages/contact_us.html'
     form_class = ContactUsForm
     success_url = reverse_lazy('pages:thanks')
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        to = 'tannermeadtv@gmail.com'
+        subject = 'Contact Us Form Submission'
+        content = f'''
+            <p>You have received a contact form submission:</p>
+            <ol>
+            '''
+        for key, value in data.items():
+            label = key.replace('_', ' ').title()
+            entry = html.escape(str(value), quote=False)
+            content += f'<li><strong>{label}:</strong> {entry}</li>'
+
+        content += '</ol>'
+
+        send_email(to, subject, content)
+        return super().form_valid(form)
 
 class ContactUsThanksView(TemplateView):
     template_name = 'pages/thanks.html'
