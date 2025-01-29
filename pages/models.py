a21e63da6091ea954be52e3a2de54a648f2c5092
Tanner Mead <tanner_mead@comcast.net>
2025-01-29 15:31:49 -0600

    created review model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    review = models.TextField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.review
