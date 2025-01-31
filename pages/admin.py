bd37a9cd77a9dd08912086397f17d043b9b7f51a
Tanner Mead <tanner_mead@comcast.net>
2025-01-31 17:25:54 -0600

    updated admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from common.admin import Play2LearnAdmin
 
-# Register your models here.
+from .models import Review
+
+@admin.register(Review)
+class ReviewAdmin(Play2LearnAdmin):
+    model = Review
+    list_display = ['review', 'first_name', 'last_name', 'created', 'user']
